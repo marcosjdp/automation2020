56a2ffeb19f8a1a93108a104424c1c07cab29ea3
Marcos Di Paola <marcosjdp@gmail.com>
2020-04-05 18:25:05 -0300

    Se termine video 3 y su ej correspondiente

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class BaseTests {
 
     private WebDriver driver;
@@ -14,8 +16,13 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        WebElement inputElement = driver.findElement(By.linkText("Inputs"));
-        inputElement.click();
+        /*WebElement inputElement = driver.findElement(By.linkText("Inputs"));
+        inputElement.click();*/
+        driver.findElement(By.linkText("Shifting Content")).click();
+        driver.findElement(By.linkText("Example 1: Menu Element")).click();
+        List<WebElement> menuItems = driver.findElements(By.tagName("li"));
+        System.out.println("Number of menu items: " + menuItems.size());
+
 
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
